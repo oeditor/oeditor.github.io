5efa51c669ae06593bd9f1fc890fe8698fb522c9
Natan Felles <natanfelles@gmail.com>
2016-12-07 18:50:37 -0200

    Up

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# oeditor.github.io
+# Online Editor
+
+A powerfull online editor, more than 110 languages read, 34 color schemes. Import from and save to your computer.
+
+* https://natanfelles.github.io/online-editor
+
+## Preview
+
+![Online Editor](https://natanfelles.github.io/assets/images_post/online-editor.png)
---
+++ assets/scripts.js
@@ -0,0 +1,79 @@
+/*!
+ * Online Editor
+ * @author Natan Felles <natanfelles@gmail.com>
+ */
+
+var hello = '  ___        _ _              _____    _ _ _             \n'
+	+ ' / _ \\ _ __ | (_)_ __   ___  | ____|__| (_) |_ ___  _ __ \n'
+	+ '| | | | \'_ \\| | | \'_ \\ / _ \\ |  _| / _` | | __/ _ \\| \'__|\n'
+	+ '| |_| | | | | | | | | |  __/ | |__| (_| | | || (_) | |   \n'
+	+ ' \\___/|_| |_|_|_|_| |_|\\___| |_____\\__,_|_|\\__\\___/|_|   \n\n'
+	+ 'by @natanfelles';
+console.log(hello);
+
+(function (i, s, o, g, r, a, m) {
+	i.GoogleAnalyticsObject = r;
+	i[r] = i[r] || function () {
+			(i[r].q = i[r].q || []).push(arguments);
+		}, i[r].l = 1 * new Date();
+	a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+	a.async = 1;
+	a.src = g;
+	m.parentNode.insertBefore(a, m);
+})(window, document, "script", "https://www.google-analytics.com/analytics.js", "ga");
+ga("create", "UA-75463090-1", "auto");
+ga("send", "pageview");
+
+ace.require("ace/ext/language_tools");
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/monokai");
+editor.session.setMode("ace/mode/php");
+editor.setOptions({
+	enableBasicAutocompletion: true,
+	enableSnippets: true,
+	enableLiveAutocompletion: true
+});
+document.getElementById("editor").style.fontSize = "16px";
+editor.$blockScrolling = Infinity;
+editor.getSession().setUseWrapMode(true);
+editor.getSession().setUseSoftTabs(true);
+editor.getSession().setTabSize(4);
+
+function saveTextAsFile() {
+	var textToWrite = editor.getValue();
+	var textFileAsBlob = new Blob([textToWrite], {
+		type: "text/plain"
+	});
+	var fileNameToSaveAs = document.getElementById("filename").value;
+	var downloadLink = document.createElement("a");
+	downloadLink.download = fileNameToSaveAs;
+	downloadLink.innerHTML = "Download File";
+	if (window.webkitURL != null) {
+		downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+	} else {
+		downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+		downloadLink.onclick = destroyClickedElement;
+		downloadLink.style.display = "none";
+		document.body.appendChild(downloadLink);
+	}
+	downloadLink.click();
+}
+
+function destroyClickedElement(event) {
+	document.body.removeChild(event.target);
+}
+
+function loadFileAsText() {
+	var fileToLoad = document.getElementById("loadfile").files[0];
+	var fileReader = new FileReader();
+	fileReader.onload = function (fileLoadedEvent) {
+		var textFromFileLoaded = fileLoadedEvent.target.result;
+		editor.setValue(textFromFileLoaded);
+		document.getElementById("filename").value = document.getElementById("loadfile").value;
+	};
+	fileReader.readAsText(fileToLoad, "UTF-8");
+}
+$(".dropdown-menu a").click(function () {
+	$(".dropdown-menu a").removeClass("active");
+	$(this).addClass("active");
+});
